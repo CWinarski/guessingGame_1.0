6adcb42c9655195270aac2e3e7508ee4c5fa8c4f
CWinarski <winarski70@yahoo.com>
2017-12-27 18:01:38 -0500

    Update After Prompt

--- GuessingGame/game.js
+++ GuessingGame/game.js
@@ -1,4 +1,5 @@
 console.log("Hello! Welcome to the Guessing Game!");
 
 var randNumber = Math.floor(Math.random()*10)+1;
-var userNumber = prompt("Please Enter in Your Guess Here: ", "");
+var prompt = require ('Guess');
+prompt.start();
