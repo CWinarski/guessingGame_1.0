f3e1a532bc7778acc186137b467a3ed1b71200ff
CWinarski <winarski70@yahoo.com>
2017-12-27 16:54:45 -0500

    Installed NPM Prompt

--- GuessingGame/game.js
+++ GuessingGame/game.js
@@ -1,3 +1,4 @@
-<script>
 console.log("Hello! Welcome to the Guessing Game!");
-</script>
+
+var randNumber = Math.floor(Math.random()*10)+1;
+var userNumber = prompt("Please Enter in Your Guess Here: ", "");
