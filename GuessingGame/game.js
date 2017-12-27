54793790fd5e1a178aa9dd7a58ac7f23f0b2925e
CWinarski <winarski70@yahoo.com>
2017-12-27 16:02:27 -0500

    Starting From Scratch

--- GuessingGame/game.js
+++ GuessingGame/game.js
@@ -1,17 +1,3 @@
+<script>
 console.log("Hello! Welcome to the Guessing Game!");
-
-var randomNumber = Math.floor(Math.random() * 10) + 1;
-var userNumber = prompt("Please enter your guess:", "");
-
-if (userNumber < randomNumber) {
-
-	console.log("Too Low!");
-	
-	if (userNumber > randomNumber) {
-	console.log("Too High!");
-	}
-	
-	if (userNumber == randomNumber){
-	console.log("You are correct! You Win!");
-	}
-}
+</script>
