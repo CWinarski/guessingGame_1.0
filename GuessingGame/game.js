5dea684b2d680f9cc8e50d01d8239b4aa3585e72
CWinarski <winarski70@yahoo.com>
2017-12-30 12:41:06 -0500

    Fully Functioning!

--- GuessingGame/game.js
+++ GuessingGame/game.js
@@ -1,27 +1,31 @@
-console.log("Hello! Welcome to the Guessing Game!");
+console.log('Hello! Welcome to the Guessing Game!');
+console.log('Please enter a number between (and including) 1 and 10');
 
 var randNumber = Math.floor(Math.random()*10)+1; //Gets a whole number including 1 and 10
-
+//var randNumber = 5; For testing the if loop 
 var prompt = require ('prompt'); 
 prompt.start();
-prompt.get(['guess'], function(err, result){
+prompt.get(['guess'], function(err, result){ // Was installed via npm install prompt
 
     console.log('Your Guess was received');
-    
-    if (result.guess < randNumber){
 
-     console.log('You are too low');
+    if ((result.guess < 1) || (result.guess > 10)){
 
-        if (result.guess > randNumber){
+        console.log('Your guess is ouside the range of numbers you can pick!');
 
-            console.log('You are too high');
+        } else if (result.guess < randNumber){
+
+          console.log('You are too low');
+
+        } else if (result.guess > randNumber){
+
+          console.log('You are too high');
+
+        } else if (result.guess == randNumber){
+
+          console.log('You guessed right! You win!');
         }
 
-        if (result.guess == randNumber){
-
-            console.log('You guessed right! You win!');
-        }
-     
-    }
+        
 });
 
