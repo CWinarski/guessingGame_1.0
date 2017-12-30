7603f7dc99bf67f1d346873ca8240dd450b31800
CWinarski <winarski70@yahoo.com>
2017-12-30 13:08:56 -0500

    GuessingGame 1.0

--- GuessingGame/game.js
+++ GuessingGame/game.js
@@ -3,6 +3,7 @@ console.log('Please enter a number between (and including) 1 and 10');
 
 var randNumber = Math.floor(Math.random()*10)+1; //Gets a whole number including 1 and 10
 //var randNumber = 5; For testing the if loop 
+
 var prompt = require ('prompt'); 
 prompt.start();
 prompt.get(['guess'], function(err, result){ // Was installed via npm install prompt
