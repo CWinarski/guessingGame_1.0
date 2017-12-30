731aac437aa746caca93f2c29fe3dc6365ac9cbe
CWinarski <winarski70@yahoo.com>
2017-12-30 11:59:56 -0500

    Basic If Added

--- GuessingGame/game.js
+++ GuessingGame/game.js
@@ -1,11 +1,27 @@
 console.log("Hello! Welcome to the Guessing Game!");
 
-var randNumber = Math.floor(Math.random()*10)+1;
-var prompt = require ('prompt');
+var randNumber = Math.floor(Math.random()*10)+1; //Gets a whole number including 1 and 10
+
+var prompt = require ('prompt'); 
 prompt.start();
 prompt.get(['guess'], function(err, result){
 
-    console.log('Your Guess was received:');
-    console.log(' Guess: ' + result.guess);
+    console.log('Your Guess was received');
+    
+    if (result.guess < randNumber){
 
+     console.log('You are too low');
+
+        if (result.guess > randNumber){
+
+            console.log('You are too high');
+        }
+
+        if (result.guess == randNumber){
+
+            console.log('You guessed right! You win!');
+        }
+     
+    }
 });
+
