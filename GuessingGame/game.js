a4547f4110bcbef1341307f176ec6d53cbaab8e9
CWinarski <winarski70@yahoo.com>
2017-12-30 10:00:05 -0500

    Prompt Works!

--- GuessingGame/game.js
+++ GuessingGame/game.js
@@ -1,5 +1,11 @@
 console.log("Hello! Welcome to the Guessing Game!");
 
 var randNumber = Math.floor(Math.random()*10)+1;
-var prompt = require ('Guess');
+var prompt = require ('prompt');
 prompt.start();
+prompt.get(['guess'], function(err, result){
+
+    console.log('Your Guess was received:');
+    console.log(' Guess: ' + result.guess);
+
+});
